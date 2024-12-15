8677909d91184f81d53683f6a72e3386b710169d
Udaykumarnaik <mudavathudaykumarnaik1431@gmail.com>
2024-12-15 12:00:07 +0530

    Code modified

--- Demo.java
+++ Demo.java
@@ -1,7 +1,24 @@
+//Single level inheritence
+
 class Chinna
+{
+	public void bank()
+	{
+		System.out.println("Chinna maintain accounts");
+	}
+}
+class Ammu extends chinna
+{
+	public void balance()
+	{
+		System.out.println("Chinna manage my accounts");
+	}
+}
+class Test
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Chinna created account");
+		Ammu a = new Ammu();
+		a.balance();
 	}
 }
